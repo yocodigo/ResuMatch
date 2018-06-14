b41b89e200b30c118629902359dd8390e7e83b12
unknown <jjhernandez94@gmail.com>
2018-06-14 09:16:10 -0700

    ignore this commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,51 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import './App.css';
+import { Grid, Container, Segment, Header, Button } from 'semantic-ui-react';
+// import ImageExampleCircular from './components/images';
+import 'semantic-ui-css/semantic.min.css';
+// import Styles from './components/button/styles.js';
+// import axios from 'axios';
 import './App.css';
 
+const outerContainerStyles = {
+  width: 'auto',
+  height: '75'
+}
+
+const innerContainerStyles = {
+  width: '90%',
+  height: '75'
+}
+
+const columnStyles = {
+  padding: '0',
+  float: 'right'
+  
+}
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Container style={ outerContainerStyles } textAlign='center'>
+        <Segment className="ui tertiary" raised>
+          <Header as='h1'>Get Giphy with React!</Header>    
+        </Segment>
+        <Container style={ innerContainerStyles } >
+          <Grid>
+            <Grid.Column width={1} style= { columnStyles }> 
+            {/*  <Button  onClick={ this.handleClick } className="ui inverted green button" style={Styles}>Giphy Me!</Button> */}
+            </Grid.Column>
+     {/* <Grid.Column>
+     <Grid columns={4} > */}
+                {this.state.giphy.map((giph) => (
+                <Grid.Column width={3}> 
+                 {/* <ImageExampleCircular src={ giph }/> */}
+                </Grid.Column>
+                ))}
+       {/*       </Grid>
+            </Grid.Column>  */}    
+          </Grid>
+        </Container>
+      </Container>
     );
   }
 }
