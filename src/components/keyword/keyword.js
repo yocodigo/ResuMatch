19d75c8f80c1e4ee8c70b058236beb1575ec2c73
unknown <jjhernandez94@gmail.com>
2018-09-13 21:53:23 -0700

    made update on dependencies

--- src/components/keyword/keyword.js
+++ src/components/keyword/keyword.js
@@ -4,8 +4,8 @@ import Upload from '../form/upload';
 const Keyword = props => (
   <div style={{background: '#46D6F7'}}>
   <br></br>
-    <h1 className="display-4 indigo-text text-darken-4" style={{padding:"0px 40px 0px 40px"}}>KEYWORD COMPARISON</h1>
-    <p className="lead indigo-text text-darken-4" style={{padding:"0px 40px 0px 40px"}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    <h1 className="display-4 indigo-text text-darken-4" style={{padding:"0px 40px 200px 40px"}}>KEYWORD <br></br> COMPARISON</h1>
+    <p className="lead indigo-text text-darken-4" style={{padding:"0px 25px 0px 40px"}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
     <br/>
   </div>
 );
--- src/components/matchrate/matchrate.js
+++ src/components/matchrate/matchrate.js
@@ -7,8 +7,8 @@ const styles = {
 
 const MatchRate = props => (
   <div>
-    <h1 className="display-4 indigo-text text-darken-4" style={{padding:"0px 40px 0px 40px"}}>MATCH RATE</h1>
-    <p className="lead indigo-text text-darken-4" style={{padding:"0px 40px 0px 40px"}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    <h1 className="display-4 indigo-text text-darken-4" style={{padding:"0px 0px 200px 40px"}}>MATCH RATE</h1>
+    <p className="lead indigo-text text-darken-4" style={{padding:"0px 25px 0px 40px"}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
     <br/>
   </div>
 );
