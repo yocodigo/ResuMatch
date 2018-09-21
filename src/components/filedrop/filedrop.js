659dfd88470265163fe92031e480056f41d0df00
unknown <jjhernandez94@gmail.com>
2018-09-20 17:16:33 -0700

    added .doc and .docx as acceptable files

--- src/components/filedrop/filedrop.js
+++ src/components/filedrop/filedrop.js
@@ -14,7 +14,7 @@ class FileDrop extends Component {
     return (
       <section className='row'>
         <div>
-          <Dropzone className='col offset-s1' style={{borderStyle:"solid", height:"350px", width:"600px", marginBottom:"20px", padding: '150px'}} onDrop={this.handleOnDrop} multiple={false} maxSize={imageMaxSize} accept='.pdf, .odt, .txt'>Drop your file here, or click submit to select file to upload</Dropzone>
+          <Dropzone className='col offset-s1' style={{borderStyle:"solid", height:"350px", width:"600px", marginBottom:"20px", padding: '150px'}} onDrop={this.handleOnDrop} multiple={false} maxSize={imageMaxSize} accept='.pdf, .odt, .txt, .doc, .docx'>Drop your file here, or click submit to select file to upload</Dropzone>
         </div>
         {/* <aside className='col s2'></aside> */}
         <aside className='col offset-s5' style={{padding:"0px"}}>
