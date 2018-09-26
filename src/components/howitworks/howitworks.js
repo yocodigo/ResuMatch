5497ef9712e299b27771fc02580039899429f126
Dima Kurbatov <dskurbatov13@gmail.com>
2018-09-25 23:00:47 -0700

    clean up

--- src/components/howitworks/howitworks.js
+++ src/components/howitworks/howitworks.js
@@ -35,21 +35,19 @@ class HowItWorks extends React.Component {
         <h1>HOW IT WORKS</h1>
         <p>We calculate your match rate and let you know how to optimize your resume</p> 
         <form className="matching">
-          <textarea onChange={this.onChange}></textarea>
+          <textarea 
+            placeholder="please copy and paste your job requirements here" 
+            onChange={this.onChange}>
+          </textarea>
           <div className="drag_and_drop"> 
             <input type="file" onChange={this.onFile}/>
           </div>
         </form>
-        <button onClick={this.onClick}><h3>Submit</h3></button>
+        <button onClick={this.onClick}><h3>Compare</h3></button>
         <span className="result"></span>
       </section>
     )
   }
 }
-
-// const HowItWorks = props => {
-
-  
-// }
 
 export default HowItWorks;
