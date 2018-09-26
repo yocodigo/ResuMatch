8ae5e0f4d6297d15763761e85d00a46be228d090
Dima Kurbatov <dskurbatov13@gmail.com>
2018-09-25 20:16:50 -0700

    redesign how itwork component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import React, { Component } from 'react';
-import Header from './components/header/Header';
+// import Header from './components/header/Header';
 import HowItWorks from './components/howitworks/howitworks';
-// import MatchRate from './components/matchrate/matchrate';
-// import Ats from './components/ats/ats';
-// import Keyword from './components/keyword/keyword';
 
 class App extends Component {
   render() {
     return (
       <main>
-        <Header />
         <HowItWorks />
-      </main>
-      
+      </main> 
     );
   }
 }
--- src/components/howitworks/howitworks.js
+++ src/components/howitworks/howitworks.js
@@ -1,12 +1,51 @@
 import React from 'react';
 import './index.css'
 
-const HowItWorks = props => (
-  <section>
-     <h1>HOW IT WORKS</h1>
-     <p>We calculate your match rate and let you know how to optimize your resume</p> 
-     <button><h3>Resume</h3></button>
-  </section>
-);
+class HowItWorks extends React.Component {
+  constructor(props){
+    super(props)
+    this.fileUpload = null
+    this.requirements = null
+
+    this.onChange = this.onChange.bind(this)
+    this.onFile = this.onFile.bind(this)
+    this.onClick = this.onClick.bind(this)
+  }
+
+  onChange(e){
+    this.requirements = e.target.value
+  }
+
+  onFile(e){
+    this.fileUpload = e.target.files
+  }
+
+  onClick(){
+    document.querySelector('textarea').textContent = ''
+    document.querySelector('.result').textContent = 'Hello'
+  }
+
+  render(){
+    return (
+      <section>
+        <h1>HOW IT WORKS</h1>
+        <p>We calculate your match rate and let you know how to optimize your resume</p> 
+        <form className="matching">
+          <textarea onChange={this.onChange}></textarea>
+          <div className="drag_and_drop"> 
+            <input type="file" onChange={this.onFile}/>
+          </div>
+        </form>
+        <button onClick={this.onClick}><h3>Submit</h3></button>
+        <span className="result"></span>
+      </section>
+    )
+  }
+}
+
+// const HowItWorks = props => {
+
+  
+// }
 
 export default HowItWorks;
