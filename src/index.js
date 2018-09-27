057b86e9cfba9db124fd8d846d068a22e0d8b9e0
Dima Kurbatov <dskurbatov13@gmail.com>
2018-09-27 11:20:57 -0700

    delete comments

--- src/index.js
+++ src/index.js
@@ -1,15 +1,9 @@
-// import 'materialize-css/dist/css/materialize.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-//add debouce or threttle
-// window.onload = () => {
-//   window.addEventListener('scroll', function(ev){
-//     console.log(ev)
-//   })
-// }
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
